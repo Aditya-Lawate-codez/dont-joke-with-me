f5d6489582d38fa900208169bb1ec805fdf75a97
Aditya-lawate-codez <aditya.211343101@vcet.edu.in>
2023-11-27 19:54:12 +0530

    initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,58 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Form, routeAction$, routeLoader$ } from '@builder.io/qwik-city';
+import styles from "./index.css?inline"
+export const useDadJoke = routeLoader$(async () => {
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
+  return (await response.json()) as {
+    id: string;
+    status: number;
+    joke: string;
+  };
+});
+
+export const useJokeVoteAction = routeAction$((props) => {
+
+  console.log('VOTE', props.vote);
+});
+
 
 export default component$(() => {
+  useStylesScoped$(styles);
+  const dadJokeSignal = useDadJoke();
+  const favoriteJokeAction = useJokeVoteAction();
+  var isFavourite = useSignal(false);
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <section class="section bright">
+      <p>{dadJokeSignal.value.joke}</p>
+      <Form action={favoriteJokeAction}>
+        <input type="hidden" onInput$={() => {
+          isFavourite.value = true
+        }} name="jokeID" value={dadJokeSignal.value.id} />
+        <button name="vote" value="up">👍</button>
+        <button name="vote" value="down">👎</button>
+      </Form>
+      <button
+        onClick$={() => {
+          isFavourite.value = !isFavourite.value;
+          console.log(isFavourite)
+        }}
+      >
+        {isFavourite.value ? "❤️" : "🤍"}
+
+      </button>
+    </section>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Don't Joke with me",
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: "Don't Joke with me",
+      content: "This is a dad's joke website",
     },
   ],
 };
